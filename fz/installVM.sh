ef22f5ea98eb228a574a4be0bbdf09ddf87f798e
Mohamed Siala <sia.mohamed@gmail.com>
2015-07-22 01:02:53 +0100

    ..

--- fz/installVM.sh
+++ fz/installVM.sh
@@ -22,7 +22,7 @@ echo "PATH=/home/user/entry_data/Mistral-2.0/fz:$PATH" >> /home/user/.bashrc
 
 cp Mistral-2.0/fz/mznlib/* mzn-lib/
 echo "Mistral-2.0 Installed.. We will performe lightweight tests. You may need to logout then login before"
-read -p "Press [Enter] key to start test"
+#read -p "Press [Enter] key to start test"
 #nano fzn-exec
 
 #
@@ -33,21 +33,21 @@ read -p "Press [Enter] key to start test"
 
 #Test if mznlib is ok! 
 
-cd
+#cd
 
-echo "exec-free black-hole 12 black-hole.mzn 12.dzn
-exec-free black-hole 6 black-hole.mzn 6.dzn
-exec-free on-call-rostering 10s-50d oc-roster.mzn 10s-50d.dzn" > entry_data/listfile
+#echo "exec-free black-hole 12 black-hole.mzn 12.dzn
+#exec-free black-hole 6 black-hole.mzn 6.dzn
+#exec-free on-call-rostering 10s-50d oc-roster.mzn 10s-50d.dzn" > entry_data/listfile
 
 
-echo "Testing Global Constraints... [only cumulative]"
+#echo "Testing Global Constraints... [only cumulative]"
 
-cd
-cp /home/user/entry_data/Mistral-2.0/fz/test--mzn-lib.sh ./
+#cd
+#cp /home/user/entry_data/Mistral-2.0/fz/test--mzn-lib.sh ./
 #wget homepages.laas.fr/msiala/minizinc2014/test--mzn-lib.sh 
-chmod +x test--mzn-lib.sh 
-./test--mzn-lib.sh 
-rm test--mzn-lib.sh 
+#chmod +x test--mzn-lib.sh 
+#./test--mzn-lib.sh 
+#rm test--mzn-lib.sh 
 #ls 
-echo "End"
+#echo "End"
 
