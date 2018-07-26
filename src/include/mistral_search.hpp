70a3fdc2e142aad946a1f8da67114c7a288bce66
ehebrard <emmanuel.hebrard@gmail.com>
2018-07-26 11:19:57 +0200

    change license to agpl

--- src/include/mistral_search.hpp
+++ src/include/mistral_search.hpp
@@ -3,19 +3,18 @@
   Mistral 2.0 is a constraint satisfaction and optimisation library
   Copyright (C) 2009  Emmanuel Hebrard
   
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
+	This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the itmplied warranty of
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
   The author can be contacted electronically at emmanuel.hebrard@gmail.com.
 */
