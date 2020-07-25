60c67eda19eee1f4406a98f7e7cf8f2356f3846d
Mohamed Siala <sia.mohamed@gmail.com>
2020-07-25 12:33:39 +0200

    Minizinc Challenge 2020
    Mistral is now available on dockerhub for minizinc challenge at https://hub.docker.com/r/msiala/mistral

--- fz/installVM.sh
+++ fz/installVM.sh
@@ -1,64 +1,51 @@
-echo "This script should be placed in the directory ~/entry_data in the Virtual Machine provided by the challenge organizers"
-
-echo "You should probably change the keyboard layout with \n dpkg-reconfigure keyboard-configuration (then restart) \n" 
-echo "github, g++, python, and make should be installed before this step"
-
+echo -e "This script is used to install the solver in the docker container used by the minizinc challenge. It should be placed in the directory ~/entry_data in the orignal minizinc chalenge container\n"
 read -p "Press [Enter] key to start installation of Mistral-2.0"
+apt-get update
+apt-get -y install wget
+cd /entry_data
+wget http://homepages.laas.fr/msiala/install.sh
+wget http://homepages.laas.fr/msiala/fzn-exec
+chmod 777 install.sh
 
-git clone --recursive https://github.com/ehebrard/Mistral-2.0.git
+apt-get -y install nano
+apt-get -y install software-properties-common
+apt-add-repository universe
+apt-get -y update
+apt-get -y install python-pip
+apt-get -y install wget
+apt-get -y install libxml2-dev
+apt-get -y install g++
+apt-get -y install git
+
+git clone https://github.com/ehebrard/Mistral-2.0.git
 cd Mistral-2.0
+git clone https://github.com/xcsp3team/XCSP3-CPP-Parser.git 
+git clone https://github.com/xcsp3team/XCSP3-Java-Tools.git
 make clean
 cd fz
 make clean
-make 
+make
 
-#To build the parallel version, uncomment the following: 
-#mv fz/mistral-fzn ../
-#make clean 
-#cd fz 
-#make clean 
-#make parallel 
-#mv ../../mistral-fzn ./
+#To build the parallel version
+mv ./mistral-fzn ../..
+cd ..
+make clean 
+cd fz 
+make clean 
+make parallel 
+mv ../../mistral-fzn ./
 
 #Here  mistral-fzn and mistral-fzn_parallel are in fz/
-
-cd /home/user/entry_data
-cp Mistral-2.0/fz/mistral-fz fzn-exec
+cd /entry_data
+#cp Mistral-2.0/fz/mistral-fz fzn-exec
+cp Mistral-2.0/fz/mistral-fzn* ./
 cp Mistral-2.0/fz/mistral-mzn ./exec
-
-chmod 777 *exec
-#echo "export PATH=\$DIR/entry_data/Mistral-2.0/fz:\$PATH" >> ../bin/challenge_env.sh
-echo "PATH=/home/user/entry_data/Mistral-2.0/fz:$PATH" >> /home/user/.bashrc 
-
+#export PATH=$PATH:/entry_data/Mistral-2.0/fz/
+#echo "PATH=/entry_data/Mistral-2.0/fz:$PATH" >> ~/.bashrc
+chmod 777 *
 cp Mistral-2.0/fz/mznlib/* mzn-lib/
-echo "Mistral-2.0 Installed. You need to logout then login to complete installation"
-echo "Please edit ~/bin/challenge_env.sh to support only free (and parallel search?)”
+echo "Mistral-2.0 Installed."
+#test :
 
-#read -p "Press [Enter] key to start test"
-#nano fzn-exec
-
-#
-#OPTIONAL :  to install X
-#apt-get install xfce4
-#apt-get install gedit
-#apt-get install Midori
-
-#Test if mznlib is ok! 
-
-#cd
-
-#echo "exec-free black-hole 12 black-hole.mzn 12.dzn
-#exec-free black-hole 6 black-hole.mzn 6.dzn
-#exec-free on-call-rostering 10s-50d oc-roster.mzn 10s-50d.dzn" > entry_data/listfile
-
-
-#echo "Testing Global Constraints... [only cumulative]"
-
-#cd
-#cp /home/user/entry_data/Mistral-2.0/fz/test--mzn-lib.sh ./
-#wget homepages.laas.fr/msiala/minizinc2014/test--mzn-lib.sh 
-#chmod +x test--mzn-lib.sh 
-#./test--mzn-lib.sh 
-#rm test--mzn-lib.sh 
-#ls 
-#echo "End"
+/minizinc/mzn-exec-free /minizinc/test.mzn /minizinc/2.dzn
+/minizinc/mzn-exec-par -p 2 /minizinc/test.mzn /minizinc/2.dzn
